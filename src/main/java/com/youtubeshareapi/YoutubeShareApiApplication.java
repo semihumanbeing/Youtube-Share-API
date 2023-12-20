da10797b1a482883177f11ff40dce90afb084819
semihumanbeing <hac.rdh96@gmail.com>
2023-12-20 16:54:01 +0900

    first commit

---
+++ src/main/java/com/youtubeshareapi/YoutubeShareApiApplication.java
@@ -0,0 +1,13 @@
+package com.youtubeshareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeShareApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(YoutubeShareApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/youtubeshareapi/YoutubeShareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtubeshareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeShareApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
